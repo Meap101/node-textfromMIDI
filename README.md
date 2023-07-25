5d5441e8c34a51023763c54554776dfbc9b821b2
Meap101 <domin8h@gmail.com>
2023-07-25 10:11:27 -0700

    Easy Keybinds

--- README.md
+++ README.md
@@ -10,4 +10,13 @@ to start the app, just run `node index.js`
 
 ## binding keys
 
-you'll have to do this manually for now, although I'll probably add a way to bind keys through the console sometime in the near future. wasn't originally planning to post this on github, so didn't think I'd need to make anything like this
+adding keybinds can be done easily directly through the console. to begin, type the word "bind" into the console (while running the application).
+
+You will be prompted to press the MIDI key you would like to bind (the keybind also keeps track of the MIDI channel used). After doing so, you will then be prompted to type the computer key you would like to bind to. Type any character into the console and you're finished.
+
+Keybinds are stored in keybinds.json, which I have pre-configured a not-so-qwerty keyboard layout into on MIDI channel 1. You may use the MIDI channels as different presets within this app.
+
+## future updates
+
+in the future i might add support for key combinations
+also in the future i may actually capitalize and punctuate the readme properly. don't push it though.
--- index.js
+++ index.js
@@ -1,211 +1,65 @@
 const midi = require('midi');
 const robot = require("robotjs");
+const fs = require('node:fs');
+
+const keybinds = require('./keybinds.json');
 
 const input = new midi.Input();
+
+var stdin = process.openStdin();
+
+var logInput = false;
+var binding = false;
+
+var bindingMidi = -1;
+var bindingChannel = -1;
 
 console.log(`Connected to ${input.getPortName(0)}`);
 
 input.on('message', (deltaTime, message) => {
 
-  console.log(`m: ${message} d: ${deltaTime}`);
+  if (logInput) {
+    console.log(`Received midi note ${message[1]} deltaTime: ${deltaTime}`);
+  }
 
-  if (message[1] == 58) {
-    if (message[0] == 144) {
-      robot.keyToggle("u", "down");
-    } else if (message[0] == 128) {
-      robot.keyToggle("u", "up");
-    }
-  } else if (message[1] == 51) {
-    if (message[0] == 144) {
-      robot.keyToggle("e", "down");
-    } else if (message[0] == 128) {
-      robot.keyToggle("e", "up");
-    }
-  } else if (message[1] == 46) {
-    if (message[0] == 144) {
-      robot.keyToggle("q", "down");
-    } else if (message[0] == 128) {
-      robot.keyToggle("q", "up");
-    }
-  } else if (message[1] == 47) {
-    if (message[0] == 144) {
-      robot.keyToggle("a", "down");
-    } else if (message[0] == 128) {
-      robot.keyToggle("a", "up");
-    }
-  } else if (message[1] == 49) {
-    if (message[0] == 144) {
-      robot.keyToggle("w", "down");
-    } else if (message[0] == 128) {
-      robot.keyToggle("w", "up");
-    }
-  } else if (message[1] == 48) {
-    if (message[0] == 144) {
-      robot.keyToggle("s", "down");
-    } else if (message[0] == 128) {
-      robot.keyToggle("s", "up");
-    }
-  } else if (message[1] == 50) {
-    if (message[0] == 144) {
-      robot.keyToggle("d", "down");
-    } else if (message[0] == 128) {
-      robot.keyToggle("d", "up");
-    }
-  } else if (message[1] == 52) {
-    if (message[0] == 144) {
-      robot.keyToggle("r", "down");
-    } else if (message[0] == 128) {
-      robot.keyToggle("r", "up");
-    }
-  } else if (message[1] == 53) {
-    if (message[0] == 144) {
-      robot.keyToggle("f", "down");
-    } else if (message[0] == 128) {
-      robot.keyToggle("f", "up");
-    }
-  } else if (message[1] == 54) {
-    if (message[0] == 144) {
-      robot.keyToggle("t", "down");
-    } else if (message[0] == 128) {
-      robot.keyToggle("t", "up");
-    }
-  } else if (message[1] == 55) {
-    if (message[0] == 144) {
-      robot.keyToggle("g", "down");
-    } else if (message[0] == 128) {
-      robot.keyToggle("g", "up");
-    }
-  } else if (message[1] == 56) {
-    if (message[0] == 144) {
-      robot.keyToggle("y", "down");
-    } else if (message[0] == 128) {
-      robot.keyToggle("y", "up");
-    }
-  } else if (message[1] == 57) {
-    if (message[0] == 144) {
-      robot.keyToggle("h", "down");
-    } else if (message[0] == 128) {
-      robot.keyToggle("h", "up");
-    }
-  } else if (message[1] == 59) {
-    if (message[0] == 144) {
-      robot.keyToggle("j", "down");
-    } else if (message[0] == 128) {
-      robot.keyToggle("j", "up");
-    }
-  } else if (message[1] == 61) {
-    if (message[0] == 144) {
-      robot.keyToggle("i", "down");
-    } else if (message[0] == 128) {
-      robot.keyToggle("i", "up");
-    }
-  } else if (message[1] == 60) {
-    if (message[0] == 144) {
-      robot.keyToggle("k", "down");
-    } else if (message[0] == 128) {
-      robot.keyToggle("k", "up");
-    }
-  } else if (message[1] == 63) {
-    if (message[0] == 144) {
-      robot.keyToggle("o", "down");
-    } else if (message[0] == 128) {
-      robot.keyToggle("o", "up");
-    }
-  } else if (message[1] == 62) {
-    if (message[0] == 144) {
-      robot.keyToggle("l", "down");
-    } else if (message[0] == 128) {
-      robot.keyToggle("l", "up");
-    }
-  } else if (message[1] == 64) {
-    if (message[0] == 144) {
-      robot.keyToggle("p", "down");
-    } else if (message[0] == 128) {
-      robot.keyToggle("p", "up");
-    }
-  } else if (message[1] == 65) {
-    if (message[0] == 144) {
-      robot.keyToggle(";", "down");
-    } else if (message[0] == 128) {
-      robot.keyToggle(";", "up");
-    }
-  } else if (message[1] == 66) {
-    if (message[0] == 144) {
-      robot.keyToggle("n", "down");
-    } else if (message[0] == 128) {
-      robot.keyToggle("n", "up");
-    }
-  } else if (message[1] == 67) {
-    if (message[0] == 144) {
-      robot.keyToggle("space", "down");
-    } else if (message[0] == 128) {
-      robot.keyToggle("space", "up");
-    }
-  } else if (message[1] == 69) {
-    if (message[0] == 144) {
-      robot.keyToggle("enter", "down");
-    } else if (message[0] == 128) {
-      robot.keyToggle("enter", "up");
-    }
-  } else if (message[1] == 41) {
-    if (message[0] == 144) {
-      robot.keyToggle("z", "down");
-    } else if (message[0] == 128) {
-      robot.keyToggle("z", "up");
-    }
-  } else if (message[1] == 42) {
-    if (message[0] == 144) {
-      robot.keyToggle("x", "down");
-    } else if (message[0] == 128) {
-      robot.keyToggle("x", "up");
-    }
-  } else if (message[1] == 43) {
-    if (message[0] == 144) {
-      robot.keyToggle("c", "down");
-    } else if (message[0] == 128) {
-      robot.keyToggle("c", "up");
-    }
-  }  else if (message[1] == 44) {
-    if (message[0] == 144) {
-      robot.keyToggle("v", "down");
-    } else if (message[0] == 128) {
-      robot.keyToggle("v", "up");
-    }
-  }  else if (message[1] == 45) {
-    if (message[0] == 144) {
-      robot.keyToggle("b", "down");
-    } else if (message[0] == 128) {
-      robot.keyToggle("b", "up");
-    }
-  } else if (message[1] == 71) {
-    if (message[0] == 144) {
-      robot.keyToggle(".", "down");
-    } else if (message[0] == 128) {
-      robot.keyToggle(".", "up");
-    }
-  } else if (message[1] == 68) {
-    if (message[0] == 144) {
-      robot.keyToggle("m", "down");
-    } else if (message[0] == 128) {
-      robot.keyToggle("m", "up");
-    }
-  } else if (message[1] == 72) {
-    if (message[0] == 144) {
-      robot.keyToggle("backspace", "down");
-    } else if (message[0] == 128) {
-      robot.keyToggle("backspace", "up");
-    }
-  } else if (message[1] == 70) {
-    if (message[0] == 144) {
-      robot.keyToggle(",", "down");
-    } else if (message[0] == 128) {
-      robot.keyToggle(",", "up");
-    }
+  if (binding) {
+    bindingMidi = message[1];
+    bindingChannel = message[0] - 143;
+
+    binding = false;
+
+    console.log("Now type the computer key you would like to bind to note to")
+  } else {
+    keybinds.map(keybind => {
+      if (message[1] === keybind.midiKey) {
+        if (message[0] === keybind.midiChannel + 143) {
+          robot.keyToggle(keybind.keyBind, "down");
+        } else if (message[0] === keybind.midiChannel + 127) {
+          robot.keyToggle(keybind.keyBind, "up");
+        }
+      }
+    })
   }
 });
 
 input.openPort(0);
 
-setTimeout(function() {
-  input.closePort();
-}, 2147483647);
+stdin.addListener("data", function(d) {
+  if (d.toString().trim() === "console") {
+    logInput = true;
+  }
+  else if (d.toString().trim() === "bind") {
+    binding = true;
+    console.log("Press the MIDI key you would like to bind")
+  }
+  else if (bindingMidi !== -1) {
+    keybinds.push({ midiKey: bindingMidi, keyBind: d.toString().trim(), midiChannel: bindingChannel})
+
+    fs.writeFile("./keybinds.json", JSON.stringify(keybinds), "utf8", (err) => {
+      if (err) { console.log(err) }
+    });
+
+    bindingMidi = -1;
+    bindingChannel = -1;
+  }
+});
